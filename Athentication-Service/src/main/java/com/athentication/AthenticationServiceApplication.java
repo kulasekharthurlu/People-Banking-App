b1ec450125ad945433b47c7c921b121605effcaa
KulashekarThurlu <thurlukulasekhar@gmail.com>
2023-02-27 15:05:16 +0530

    initial commit

---
+++ Athentication-Service/src/main/java/com/athentication/AthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.athentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ Athentication-Service/src/test/java/com/athentication/AthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
